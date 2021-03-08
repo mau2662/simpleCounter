c7d9436329e93d21f9a918e182c108c6ef26af8b
mau2662 <vargas7384@gmail.com>
2021-03-08 03:08:03 +0000

    contador listo, faltan los ceros

--- src/js/component/Seconds.js
+++ src/js/component/Seconds.js
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 const Seconds = props => {
 	const format = () => {
-		let resultado = "00" + props.seconds;
+		let resultado = props.seconds + "";
 		return resultado;
 	};
 	return (
 		<div className="text-center mt-5 bg-dark text-light">
 			<h1 className="display-1">
-				<i className="fas fa-clock"></i> {props.seconds}
+				<i className="fas fa-clock"> </i> {props.seconds}
 			</h1>
 		</div>
 	);
