e7f83a79a43d4ad0f92ed28bfe2c0d2b783aa174
mau2662 <vargas7384@gmail.com>
2021-03-08 02:25:24 +0000

    reloj

--- src/js/component/Seconds.js
+++ src/js/component/Seconds.js
@@ -5,7 +5,13 @@ const Seconds = props => {
 		let resultado = "00" + props.seconds;
 		return resultado;
 	};
-	return <div> {format()}</div>;
+	return (
+		<div className="text-center mt-5 bg-dark text-light">
+			<h1 className="display-1">
+				<i className="fas fa-clock"></i> {props.seconds}
+			</h1>
+		</div>
+	);
 };
 
 Seconds.propTypes = {
